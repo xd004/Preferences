460fd456971d23ee519fd03d01a6e09f266552af
Xiaying Deng <xiaying.deng123@gmail.com>
2022-03-15 23:43:17 -0500

    gambles only

--- gamblesonly.R
+++ gamblesonly.R
@@ -3,38 +3,30 @@ library(qualtRics)
 library(tidyverse)
 source("./vpcR.R")
 library(lme4)
+library(kableExtra)
 
-df <- qualtRics::read_survey("./gamblesver2100.csv")
+df <- qualtRics::read_survey("./gambles.csv")
 
 # 40 - 81, gamble 1
 # 1 - 39, 4000, 4100, 4200, gamble 2
-# 149 - 190, face 1
-# 403 - 444, face 2
+
 data <- df %>% 
   select(`ResponseId`, starts_with("Q")) %>% 
-  select(-c(2,3,4,5,6,7,8,9,"Q25","Q26_1...203","Q27","Q28","Q197","Q272_11","Q273_11","Q274_11","Q84_11","Q185_11","Q186_11"))%>%
+  select(-c(2,3,4,5,6,7,8,9,"Q25","Q27","Q28","Q26_1...119","Q197","Q272_11","Q273_11","Q274_11","Q84_11","Q185_11","Q186_11"))%>%
   pivot_longer(
     cols = -ResponseId,
     names_to = "question",
     values_to = "response"
   )%>%
   mutate(num = parse_number(question))%>%
-  mutate(type = case_when(
-    between(num, 40, 81) ~ "gamble",
-    between(num, 149, 190) ~ "face",
-    between(num, 403, 444) ~ "face",
-    between(num, 1, 4200) ~ "gamble",
-  ))%>%
   mutate(block = case_when(
     between(num, 40, 81) ~ "1",
-    between(num, 149, 190) ~ "1",
-    between(num, 403, 444) ~ "2",
-    between(num, 1, 4200) ~ "2",
+    between(num, 1, 4200) ~ "2"
   ))%>%
   mutate(participant = `ResponseId`)
 
 fit1 <- lme4::lmer(
-  response ~ 1 + (1|participant) + (1|type) + (1|participant:block) + (1|participant:type),
+  response ~ 1 + (1|participant)+ (1|block) + (1|participant:block),
   data = data,
   control = lmerControl(optimizer = 'bobyqa'),
   REML = TRUE
@@ -54,9 +46,10 @@ vpc1 <- compute_vpc(
   data,
   glmer = FALSE,
   dv = "response",
-  idyo_ivs = c("participant", "participant:type"),
-  shared_ivs = c("type"),
+  idyo_ivs = c("participant"),
+  shared_ivs = c("block"),
   additional_ivs = c("participant:block")
 )
 
 vpc1
+
