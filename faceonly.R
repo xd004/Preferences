2ed0609e3c761204d6f03b9fab3234d22130310a
Xiaying Deng <xiaying.deng123@gmail.com>
2022-03-18 18:14:31 -0500

    faces

--- faceonly.R
+++ faceonly.R
@@ -4,13 +4,13 @@ library(tidyverse)
 source("./vpcR.R")
 library(lme4)
 library(kableExtra)
-
+df <- qualtRics::read_survey("./faceonly.csv")
 # 40 - 81, gamble 1
 # 1 - 39, 4000, 4100, 4200, gamble 2
-df <- qualtRics::read_survey("./gamblesonly.csv")
+
 data <- df %>% 
   select(`ResponseId`, starts_with("Q")) %>% 
-  select(-c(2,3,4,5,6,7,8,9,"Q25","Q27","Q28","Q26_1...119","Q197","Q272_11","Q273_11","Q274_11","Q84_11","Q185_11","Q186_11"))%>%
+  select(-c(2,3,4,5,6,7,8,9,"Q25","Q26_1","Q27","Q28","Q197","Q272_11","Q273_11","Q274_11","Q84_11","Q185_11","Q186_11"))%>%
   pivot_longer(
     cols = -ResponseId,
     names_to = "question",
@@ -18,8 +18,8 @@ data <- df %>%
   )%>%
   mutate(num = parse_number(question))%>%
   mutate(block = case_when(
-    between(num, 40, 81) ~ "1",
-    between(num, 1, 4200) ~ "2"
+    between(num, 149, 190) ~ "1",
+    between(num, 403, 444) ~ "2"
   ))%>%
   mutate(participant = `ResponseId`)
 
